f2195601785c38d1ea7bdaba78a28f7281975928
Brent Klein <brentcklein@gmail.com>
2021-05-27 23:05:49 -0400

    simple fuzzy scryfall image search

--- src/main/scala/com/brentcklein/Main.scala
+++ src/main/scala/com/brentcklein/Main.scala
@@ -6,6 +6,25 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import scala.io.StdIn
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json.DefaultJsonProtocol
+import scala.concurrent.Future
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.unmarshalling.Unmarshal
+
+case class MessageIn(text: String)
+case class MessageOut(text: String, bot_id: String)
+
+case class ScryfallResponseImageUris(normal: String)
+case class ScryfallResponse(image_uris: ScryfallResponseImageUris)
+
+object JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val messageInFormat = jsonFormat1(MessageIn)
+  implicit val messageOutFormat = jsonFormat2(MessageOut)
+
+  implicit val scryfallResponseImageUrisFormat = jsonFormat1(ScryfallResponseImageUris)
+  implicit val scryfallResponseFormat = jsonFormat1(ScryfallResponse)
+}
 
 object Main {
 
@@ -15,10 +34,44 @@ object Main {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.executionContext
 
+    import JsonSupport._
+
     val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      pathEndOrSingleSlash {
+        post {
+          entity(as[MessageIn]) { message => // will unmarshal JSON to Message
+            val text = message.text
+            // check to see whether there are double square brackets
+            // if so, grab the text inside
+            // call scryfall api and get image link using name
+            val scryfallQuery = ???
+            val f: Future[HttpResponse] = Marshal(scryfallQuery).to[RequestEntity].flatMap { entity =>
+              Http().singleRequest(
+                HttpRequest(
+                  uri = Uri("https://api.scryfall.com/cards/named").withQuery(Uri.Query(("fuzzy" -> text))),
+                )
+              )
+            }.flatMap{ response =>
+              Unmarshal(response).to[ScryfallResponse]
+            }.flatMap{scryfallResponse =>
+              // post link text to thread with card name
+              val responseMessage = MessageOut(scryfallResponse.image_uris.normal, "c05d3805b3d555a1837209bab8")
+              Marshal(responseMessage).to[RequestEntity].flatMap { entity =>
+                Http().singleRequest(  
+                  HttpRequest(
+                    uri = "https://api.groupme.com/v3/bots/post",
+                    method = HttpMethods.POST,
+                    entity = entity
+                  )
+                )
+              }
+            }
+
+            // if no card found on scryfall, post snarky message to thread
+            complete(f)
+          }
+        } ~ get {
+          complete("hello")
         }
       }
 
